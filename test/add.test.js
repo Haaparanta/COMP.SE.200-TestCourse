ad5a45c9a533861b069af2f7218c341b30cfae7a
vesa haaparanta <vesahaaparanta@hotmail.fi>
2023-11-21 20:16:51 +0200

    added capitalize.js testing

--- test/add.test.js
+++ test/add.test.js
@@ -1,9 +1,39 @@
 import add from '../src/add.js';
 
+describe('Add function', () => {
+  it('Should add two positive numbers correctly', () => {
+    expect(add(6, 4)).toBe(10);
+    expect(add(2, 3)).toBe(5);
+    expect(add(1, 1)).toBe(2);
+  });
 
-describe('add', () => {
+  it('Should add two negative numbers correctly', () => {
+    expect(add(-2, -3)).toBe(-5);
+    expect(add(-1, -1)).toBe(-2);
+    expect(add(-1, 1)).toBe(0);
+  });
 
-  it('Placeholder test', () => {
-      expect(true).toBe(true);
+  it('Should add decimal numbers correctly', () => {
+    expect(add(2.5, 3.5)).toBe(6);
+    expect(add(1.1, 1.2)).toBeCloseTo(2.3);
+  });
+
+  it('Should handle addition with zero correctly', () => {
+    expect(add(0, 5)).toBe(5);
+    expect(add(3, 0)).toBe(3);
+    expect(add(0, 0)).toBe(0);
+  });
+
+  it('Should add large numbers correctly', () => {
+    expect(add(1000000000, 1000000000)).toBe(2000000000);
+    expect(add(500000000, 500000000)).toBe(1000000000);
+  });
+
+  // it should handle non-numeric inputs with a throw error and not combining the inputs
+  // function add parameters 
+  it('Should handle non-numeric inputs', () => {
+    expect(() => add('a', 'b')).toThrow();
+    expect(() => add(undefined, null)).toThrow();
+    expect(() => add(true, false)).toThrow();
   });
 });
--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -1,9 +1,93 @@
 import capitalize from '../src/capitalize.js';
 
 
-describe('capitalize', () => {
+describe('Capitalize', () => {
 
-  it('Placeholder test', () => {
-      expect(true).toBe(true);
+  test('Capitalizes basic string', () => {
+    expect(capitalize('hello')).toBe('Hello');
+    expect(capitalize('Hello')).toBe('Hello');
   });
+
+  test('String with Uppercase Characters', () => {
+    expect(capitalize('hELLO')).toBe('Hello');
+    expect(capitalize('HELLO')).toBe('Hello');
+    expect(capitalize('HeLlO')).toBe('Hello');
+  });
+
+  test('String with special characters', () => {
+    expect(capitalize('hello!')).toBe('Hello!');
+    expect(capitalize('hello!')).toBe('Hello!');
+    expect(capitalize('hello?')).toBe('Hello?');
+    expect(capitalize('hello?')).toBe('Hello?');
+    expect(capitalize('hello.')).toBe('Hello.');
+    expect(capitalize('hello.')).toBe('Hello.');
+  });
+
+  test('Empty String', () => {
+    expect(capitalize('')).toBe('');
+  });
+
+  test('String with Leading Spaces', () => {
+    expect(capitalize(' hello')).toBe(' Hello');
+    expect(capitalize('  hello')).toBe('  Hello');
+  });
+
+  test('String with Trailing Spaces', () => {
+    expect(capitalize('hello ')).toBe('Hello ');
+    expect(capitalize('hello  ')).toBe('Hello  ');
+  });
+
+  test('String with Leading and Trailing Spaces', () => {
+    expect(capitalize(' hello ')).toBe(' Hello ');
+    expect(capitalize('  hello  ')).toBe('  Hello  ');
+  });
+
+  test('String with a single character', () => {
+    expect(capitalize('h')).toBe('H');
+    expect(capitalize('H')).toBe('H');
+  });
+
+  test('String with a sentence', () => {
+    expect(capitalize('hello world')).toBe('Hello world');
+    expect(capitalize('Hello world')).toBe('Hello world');
+  });
+
+  test('String with a numbers', () => {
+    expect(capitalize('hello 123')).toBe('Hello 123');
+    expect(capitalize('Hello 123')).toBe('Hello 123');
+    expect(capitalize('123 hello')).toBe('123 hello');
+  });
+
+  test('Null', () => {
+    expect(capitalize(null)).toBe('');
+  });
+
+  test('Undefined', () => {
+    expect(capitalize(undefined)).toBe('');
+  });
+
+  test('Boolean', () => {
+    expect(capitalize(true)).toBe('');
+    expect(capitalize(false)).toBe('');
+  });
+
+  test('Object', () => {
+    expect(capitalize({})).toBe('');
+  });
+
+  test('Array', () => {
+    expect(capitalize([])).toBe('');
+    expect(capitalize(['hello'])).toBe('hello');
+    expect(capitalize([1, 2, 3])).toBe('123');
+  });
+
+  test('Number', () => {
+    expect(capitalize(123)).toBe('');
+    expect(capitalize(123)).toBe('');
+  });
+
+  test('Function', () => {
+    expect(capitalize(() => {})).toBe('');
+  });
+  
 });
