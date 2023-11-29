f3ad7ab40cc2187eb0f45646a4ef2f1215b8867c
Kamu Malinen <kamu.malinen@tuni.fi>
2023-11-29 17:09:43 +0200

    Add filter.js tests.

--- test/filter.test.js
+++ test/filter.test.js
@@ -1,9 +1,37 @@
-//import filter from '../src/filter.js';
+import filter from '../src/filter.js';
 
 
 describe('filter', () => {
+  let original = [];
 
-  it('Placeholder', () => {
-      expect(true).toBe(true);
+  beforeEach(() => {
+    // Create array of [0..n] where n is in [10..19]
+    original = [...Array(Math.floor((Math.random() + 1) * 10)).keys()];
+  });
+
+  test('returns an array', () => {
+    expect(filter(original, (_=>true))).toBeInstanceOf(Array);
+  });
+
+  test('returns a filtered array', () => {
+    expect(filter(original, (_ => _ % 2))).toEqual(original.filter(_ => _ % 2))
+  });
+
+  test('returns an empty array when one is passed', () => {
+    // Neither pass
+    // expect(filter([], (_ => true))).toEqual([]);
+    expect(filter([], (_ => false))).toEqual([]);
+  });
+
+  test('doesn\'t modify the original array', () => {
+    let copy = structuredClone(original);
+
+    filter(original, (_ => _ % 2))
+
+    expect(original).toEqual(copy);
+  });
+
+  test('filters nothing when predicate is true for all', () => {
+    expect(filter(original, (_ => true))).toEqual(original);
   });
 });
