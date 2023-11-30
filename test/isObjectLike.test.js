459eaccbd413e5b96624ec30f077656a8c37f755
Kamu Malinen <kamu.malinen@tuni.fi>
2023-11-30 18:07:20 +0200

    Add isObjectLike.js tests.

--- test/isObjectLike.test.js
+++ test/isObjectLike.test.js
@@ -1,9 +1,38 @@
-//import isObjectLike from "../src/isObjectLike";
+import isObjectLike from "../src/isObjectLike";
 
 
 describe('isObjectLike', () => {
 
-  it('Placeholder', () => {
-      expect(true).toBe(true);
+  test('returns true for object', () => {
+    expect(isObjectLike({'a' : undefined})).toBe(true);
+  });
+
+  // Test all primitive types.
+  test('returns false for number type', () => {
+    expect(isObjectLike(Math.random())).toBe(false);
+  });
+
+  test('returns false for bigint type', () => {
+    expect(isObjectLike(BigInt(Math.floor(Math.random()*10)))).toBe(false);
+  });
+
+  test('returns false for boolean type', () => {
+    expect(isObjectLike(true)).toBe(false);
+  });
+
+  test('returns false for null type', () => {
+    expect(isObjectLike(null)).toBe(false);
+  });
+
+  test('returns false for undefined type', () => {
+    expect(isObjectLike(undefined)).toBe(false);
+  });
+
+  test('returns false for string type', () => {
+    expect(isObjectLike(Math.random.toString())).toBe(false);
+  });
+
+  test('returns false for symbol type', () => {
+    expect(isObjectLike(Symbol("foo"))).toBe(false);
   });
 });
